0b453f6c62181db1c1a5d70b6f0964b043a9fe41
Ara-O <oladipoeyiara@gmail.com>
2025-06-01 19:25:28 -0400

    working more on UI

--- ai-asl/src/views/Home.vue
+++ ai-asl/src/views/Home.vue
@@ -13,36 +13,95 @@
         class="w-full h-[calc(100vh-140px)] bg-gray-100 rounded-lg"
       ></video>
     </section>
-    <section class="rounded-lg flex">
+    <section class="rounded-lg flex flex-col">
+      <!-- <p class="h-25">Modes</p> -->
       <div class="h-30 flex items-center w-full">
-        <div class="shadow-sm w-full justify-around rounded-full flex items-center h-20 gap-4">
-          <p class="font-semibold text-gray-600">Modes</p>
+        <div
+          class="shadow-sm w-full overflow-auto justify-around rounded-full flex items-center h-20 gap-4"
+        >
           <div class="flex items-center gap-4">
-            <span class="w-22 flex flex-col items-center h-13 justify-between">
-              <img src="@/assets/alphabet-level.png" alt="Alphabet level" class="w-7" />
+            <span
+              class="w-22 flex flex-col items-center h-13 justify-between cursor-pointer"
+              @click="selection = 'Alphabet'"
+            >
+              <img
+                src="@/assets/alphabet-level.png"
+                alt="Alphabet level"
+                class="w-8 rounded-full transition-all duration-250 border p-1 border-white"
+                :class="{ selected: selection === 'Alphabet' }"
+              />
               <p class="text-[12px] font-medium text-gray-600 text-center">Alphabet level</p>
             </span>
-            <span class="w-22 flex flex-col items-center h-13 justify-between">
-              <img src="@/assets/word-level.png" alt="Word level" class="w-8.5" />
+            <span
+              class="w-22 flex flex-col items-center h-13 justify-between cursor-pointer"
+              @click="selection = 'Word'"
+            >
+              <img
+                src="@/assets/word-level.png"
+                alt="Word level"
+                class="w-8 rounded-full transition-all duration-250 border p-1 border-white"
+                :class="{ selected: selection === 'Word' }"
+              />
               <p class="text-[12px] font-medium text-gray-600 text-center">Word level</p>
             </span>
-            <span class="w-22 flex flex-col items-center h-13 gap-3">
-              <img src="@/assets/lip-reading.png" alt="Lip reading" class="w-4" />
+            <span
+              class="w-22 flex flex-col items-center h-13 justify-between cursor-pointer"
+              @click="selection = 'Lip'"
+            >
+              <img
+                src="@/assets/lip-reading.png"
+                alt="Lip reading"
+                class="w-8 rounded-full transition-all duration-250 border p-1 border-white"
+                :class="{ selected: selection === 'Lip' }"
+              />
               <p class="text-[12px] font-medium text-gray-600 text-center">Lip reading</p>
             </span>
-            <span class="w-22 flex flex-col items-center h-13 gap-3">
-              <img src="@/assets/emotion-reading.png" alt="Emotion detection" class="w-8" />
-              <p class="text-sm font-medium text-gray-600 text-center">Emotion detection</p>
+            <span
+              class="w-22 flex flex-col items-center h-13 justify-between cursor-pointer"
+              @click="selection = 'Emotion'"
+            >
+              <img
+                src="@/assets/emotion-reading.png"
+                alt="Emotion detection"
+                class="w-8 rounded-full transition-all duration-250 border p-1 border-white"
+                :class="{ selected: selection === 'Emotion' }"
+              />
+              <p class="text-[12px] font-medium text-gray-600 text-center">Emotion</p>
             </span>
           </div>
         </div>
       </div>
+      <article class="h-83 w-full rounded-lg py-5 flex items-center flex-col justify-between">
+        <div class="flex justify-between w-full">
+          <p class="font-medium">Prediction:</p>
+          <p class="font-medium">Confidence: 10%</p>
+        </div>
+        <p class="text-9xl my-0">{{ prediction }}</p>
+        <progress class="w-[90%] rounded-full" value="10" max="100"></progress>
+      </article>
+      <section class="mt-10">
+        <p class="font-medium">Other possible predictions:</p>
+        <div class="mt-4 flex gap-4 flex-wrap">
+          <div class="max-w-36 bg-white px-3 rounded-md py-3">
+            <p class="font-medium">S</p>
+            <progress class="w-full rounded-full mt-4" value="5" max="100"></progress>
+          </div>
+          <div class="max-w-36 bg-white px-3 rounded-md py-3">
+            <p class="font-medium">T</p>
+            <progress class="w-full rounded-full mt-4" value="50" max="100"></progress>
+          </div>
+        </div>
+      </section>
     </section>
   </main>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
+
+type Modes = 'Alphabet' | 'Word' | 'Lip' | 'Emotion'
+const prediction = ref<string>('A')
+const selection = ref<Modes>('')
 
 onMounted(() => {
   const video = document.getElementById('video') as HTMLVideoElement
@@ -61,3 +120,11 @@ onMounted(() => {
   }
 })
 </script>
+
+<style>
+.selected {
+  padding: 4px;
+  border: solid 1px rgb(203, 203, 203);
+  border-radius: 100px;
+}
+</style>
